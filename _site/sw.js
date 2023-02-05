ee5aeb4bc1b24373ae0ca9d680d29e32a199d17b
Kushal Basu <kushalbasu2@gmail.com>
2023-02-05 21:12:48 +0530

    fix

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230205.231646';
+const cacheName = 'chirpy-20230205.234143';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
